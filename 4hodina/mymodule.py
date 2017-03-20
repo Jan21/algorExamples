27d5226cd0fcb86bd278b78d911bffaa1a00a4b4
Jan Hula <jan.hula21@gmail.com>
2017-03-20 11:08:08 +0100

    4 hodina

---
+++ 4hodina/mymodule.py
@@ -0,0 +1,18 @@
+## tento řádek uloží tuto buňku do souboru mymodule.py
+"""
+Ukázka jednoduchého modulu
+"""
+
+my_variable = 0
+
+def my_function():
+    """
+    jednoduchá funkce
+    """
+    return my_variable
+
+def my_function2(mystring):
+    """
+    jednoduchá funkce s argumentem
+    """
+    print(mystring)
